3136262ff1051ba6f733ceb709123758d8e5a983
Abdulrahman Tolba <abetolba@gmail.com>
2019-06-06 10:47:54 -0400

    Seperated index and component logic

---
+++ markdown-editor/src/components/MarkdownEditor.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Remarkable from "remarkable";
+
+/**
+ * This component renders a textarea that transforms markdown 
+ * syntax to markdown. For a guide to markdown, refer to: 
+ * https://guides.github.com/features/mastering-markdown/
+ * @author abdultolba
+ */
+class MarkdownEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "Type some *markdown* here!" };
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  getRawMarkup() {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value) };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="input">
+          <h3>Input</h3>
+          <textarea
+            className="input-text"
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+          />
+        </div>
+        <div className="output">
+          <h3>Markdown</h3>
+          <div
+            dangerouslySetInnerHTML={this.getRawMarkup()}
+            className="output-text"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MarkdownEditor;
