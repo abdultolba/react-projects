bc80774b4c2df225dcb0ee468bc8cfdf2000e8bf
Abdulrahman Tolba <abetolba@gmail.com>
2019-06-13 23:35:14 -0400

    Added todo

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../api/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    // TODO: Use middleware to properly fetch posts
+    const response = await jsonPlaceholder.get("/posts");
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
