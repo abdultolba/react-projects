28543f957769069ce18851058f8ee59443f36c7f
Abdulrahman Tolba <abetolba@gmail.com>
2019-06-27 13:56:51 -0400

    Simple file formatting :)

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,14 +1,14 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = userId => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
 };
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
-}
+  return {
+    type: SIGN_OUT
+  };
+};
